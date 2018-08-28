7b5b5602652192196e37ecbb680e24b2d857ba37
Tiago Gimenes <tlgimenes@gmail.com>
2018-08-28 10:11:43 -0300

    Saves app config by graphiql

--- node/resolvers/saveSettings.ts
+++ node/resolvers/saveSettings.ts
@@ -1,13 +1,21 @@
 import { ForbiddenError } from 'apollo-server-errors'
+import deepmerge from 'deepmerge'
 import { checkoutAppId, checkoutAppLocator } from '../resources/conf'
+
+const arrayMerge = (_, source) => source
 
 export const saveSettings = async (_, args: {settings: Settings}, ctx: ColossusContext): Promise<Settings | void> => {
   const caller = ctx.get('x-vtex-caller')
-  const {settings} = args
+  const {settings: newSettings} = args
 
-  if (caller === checkoutAppId) {
-    return ctx.apps.saveAppSettings(checkoutAppLocator, settings)
-  }
+  const oldSettings = await ctx.apps.getAppSettings(checkoutAppLocator) as Settings
+  const settings: Settings = deepmerge(oldSettings, newSettings, {arrayMerge})
+  return ctx.apps.saveAppSettings(checkoutAppLocator, settings)
+
+  // console.log(settings)
+
+  // if (caller === checkoutAppId) {
+  // }
 
   throw new ForbiddenError(`Caller ${caller} is different from appId ${checkoutAppId}`)
 }
--- node/resolvers/settings.ts
+++ node/resolvers/settings.ts
@@ -4,9 +4,9 @@ import {checkoutAppId, checkoutAppLocator} from '../resources/conf'
 export const settings = async (_, __, ctx: ColossusContext): Promise<Settings | void> => {
   const caller = ctx.get('x-vtex-caller')
 
-  if (caller === checkoutAppId) {
+  // if (caller === checkoutAppId) {
     return ctx.apps.getAppSettings(checkoutAppLocator)
-  }
+  // }
 
   throw new ForbiddenError(`Caller ${caller} is different from appId ${checkoutAppId}`)
 }
