a62e4ff851a258e630650c7738a18cd165e8b6ab
tiagonapoli <napoli.tiago96@gmail.com>
2019-01-29 18:53:06 -0200

    Fix graphql call. Get utm from url

--- react/index.tsx
+++ react/index.tsx
@@ -4,7 +4,23 @@ import { Query } from 'react-apollo'
 import HtmlPageBuilder from './core/HtmlPageBuilder'
 import CheckoutHtml from './queries/CheckoutHtml.graphql'
 
+const getUtm = (props: any): UTM => {
+  const { query } = props
+  return {
+    campaign: query.utm_campaign || null,
+    medium: query.utm_medium || null,
+    source: query.utm_source || null,
+  }
+}
+
 class Checkout extends Component {
+
+  private utm: UTM
+
+  constructor(props: any) {
+    super(props)
+    this.utm = getUtm(props)
+  }
 
   public shouldComponentUpdate() {
     return false
@@ -12,7 +28,7 @@ class Checkout extends Component {
 
   public render () {
     return (
-        <Query query={CheckoutHtml} variables={ {major: 5, environment: 'stable'} }>
+        <Query query={CheckoutHtml} variables={ {major: 5, environment: 'stable', utm: this.utm } }>
           {({ loading, error, data}) => {
             if(loading || error) {
               return ''
