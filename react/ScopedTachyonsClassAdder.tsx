a190c57b746cc39bbb7cea8d6fe3907d95208f99
tiagonapoli <napoli.tiago96@gmail.com>
2019-02-15 11:44:15 -0200

    Add scoped tachyons class adder

---
+++ react/ScopedTachyonsClassAdder.tsx
@@ -0,0 +1,42 @@
+import React, { Component, Fragment } from 'react'
+
+const addScopedTachyonsClass = (querySelector: string) => {
+    const el = document.querySelector(`${querySelector}`)
+    console.log("CLIENT", el)
+    if(el) {
+      el.classList.add('scoped-tachyons')
+    }
+}
+
+const addScopedTachyonsClassSSR = (querySelector: string) => `
+  (function() {
+    const el = document.querySelector("${querySelector}");
+    if(el) {
+      el.classList.add('scoped-tachyons')
+    }
+  })()
+`
+
+class ScopedTachyonsClassAdder extends Component {
+
+  public componentDidMount() {
+    this.props.selectors.forEach(selector => {
+      addScopedTachyonsClass(selector)
+    })
+  }
+
+  public render() {
+    return (
+      <Fragment>
+        {
+          this.props.selectors.map((selector, id) =>
+            <script key={id} dangerouslySetInnerHTML={ { __html: addScopedTachyonsClassSSR(selector) } }/>
+          )
+        }
+      </Fragment>
+
+    )
+  }
+}
+
+export default ScopedTachyonsClassAdder
