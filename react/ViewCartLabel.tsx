d610b55c6222ed4ed278d932f73e8ea8de16fb35
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-03-25 17:50:36 -0300

    add view cart button in review step

--- react/ViewCartLabel.tsx
+++ react/ViewCartLabel.tsx
@@ -1,14 +1,24 @@
+import classNames from 'classnames'
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { useCssHandles } from 'vtex.css-handles'
 
 const CSS_HANDLES = ['viewCartLabel'] as const
 
-const ViewCartLabel: React.FC = () => {
+interface Props {
+  variant?: 'primary' | 'normal'
+}
+
+const ViewCartLabel: React.FC<Props> = ({ variant = 'normal' }) => {
   const handles = useCssHandles(CSS_HANDLES)
 
+  const classes = classNames(handles.viewCartLabel, 't-body f6 fw6', {
+    'c-muted-1': variant === 'normal',
+    'c-action-primary': variant === 'primary',
+  })
+
   return (
-    <span className={`c-muted-1 t-body f6 fw6 ${handles.viewCartLabel}`}>
+    <span className={classes}>
       <FormattedMessage id="store/view-cart-label" />
     </span>
   )
