83008f70dbdb20faa642b2a36c8d0a3dff6d381f
Lucas Cordeiro <ecdb.lucas@gmail.com>
2022-02-07 16:02:43 -0300

    Add account name as querystring on proxy

--- node/clients/fastcheckout.ts
+++ node/clients/fastcheckout.ts
@@ -1,13 +1,21 @@
 import { ExternalClient, IOContext } from '@vtex/api'
 
+const BASE_URL = 'http://checkout.vtex.io'
+
 export class FastCheckoutClient extends ExternalClient {
   constructor(context: IOContext) {
-    super('http://checkout.vtex.io', context, {
+    super(BASE_URL, context, {
       timeout: 10_000,
     })
   }
 
   public getPage(pathname: string): Promise<string> {
-    return this.http.get(pathname, { headers: { 'X-Vtex-Use-Https': 'true' } })
+    const url = new URL(pathname, BASE_URL)
+
+    url.searchParams.set('an', this.context.account)
+
+    return this.http.get(url.pathname + url.search, {
+      headers: { 'X-Vtex-Use-Https': 'true' },
+    })
   }
 }
